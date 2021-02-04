0e40709a5966390fa6f24e983f8592b916b6c559
Tiramister <nh111424@gmail.com>
2021-02-04 23:52:12 +0900

    create

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,4 +2,20 @@ class MemosController < ApplicationController
   def index
     @data = Memo.all
   end
+
+  def create
+    if request.post?
+      Memo.create(memo_params)
+    end
+    redirect_to '/memos'
+  end
+
+  def add
+    @memo = Memo.new
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:name, :url, :memo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'memos/index'
   get 'memos', to: 'memos#index'
+
+  get 'memos/add'
+  post 'memos/create'
 end
