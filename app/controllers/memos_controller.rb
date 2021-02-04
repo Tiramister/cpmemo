25e1c0a2c98da5cc6cad12c79606260c9af4af56
Tiramister <nh111424@gmail.com>
2021-02-05 00:27:23 +0900

    show, edit, delete

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,15 +3,33 @@ class MemosController < ApplicationController
     @data = Memo.all
   end
 
-  def create
-    if request.post?
-      Memo.create(memo_params)
-    end
-    redirect_to '/memos'
+  def show
+    @memo = Memo.find(params[:id])
   end
 
   def add
     @memo = Memo.new
+  end
+
+  def create
+    obj = Memo.create(memo_params)
+    redirect_to '/memos/' + obj.id.to_s
+  end
+
+  def edit
+    @memo = Memo.find(params[:id])
+  end
+
+  def update
+    obj = Memo.find(params[:id])
+    obj.update(memo_params)
+    redirect_to '/memos/' + obj.id.to_s
+  end
+
+  def delete
+    obj = Memo.find(params[:id])
+    obj.destroy
+    redirect_to '/memos/'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   get 'memos/add'
   post 'memos/create'
+
+  get 'memos/edit/:id', to: 'memos#edit'
+  patch 'memos/update/:id', to: 'memos#update'
+
+  get 'memos/delete/:id', to: 'memos#delete'
+
+  get 'memos/:id', to: 'memos#show'
 end
